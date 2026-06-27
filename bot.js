dcda196fa2c047403789c3680924abd15db0b6ab
truekid-jpg <truekid@sky.com>
2026-06-27 01:50:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -64,7 +64,7 @@ const commands = [
 
 client.once("clientReady", async () => {
   console.log(`Logged in as ${client.user.tag}`);
-  client.user.setActivity("Auto-deleting messages", { type: 0 });
+  client.user.setActivity("Auto-deleting messages", { type: 3 });
   const rest = new REST({ version: "10" }).setToken(TOKEN);
   await rest.put(Routes.applicationCommands(client.user.id), { body: commands });
   console.log("Slash commands registered.");
